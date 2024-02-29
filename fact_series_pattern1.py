280d501470447cf5422fa35b071d3593edb901da
VarunGyanchandani <116818540+VarunGyanchandani@users.noreply.github.com>
2024-02-29 23:27:38 +0530

    Add files via upload

---
+++ fact_series_pattern1.py
@@ -0,0 +1,8 @@
+#1/1!+1/2!+1/3!+…1/N!
+
+import math
+n=int(input("Enter the number of terms: "))
+sum1=1
+for i in range(1,n+1):
+    sum1=sum1+(1/math.factorial(i))
+print("The sum of series is",round(sum1,2))
